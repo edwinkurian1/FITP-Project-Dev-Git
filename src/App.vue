6c302cf0689474672b58a6c228dca3fc7f237b23
Kamran7861 <107020981+Kamran7861@users.noreply.github.com>
2024-03-04 13:49:18 -0600

    Login Page
    -> I created a front end login page where the user can come and login using their credentials

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <login-form></login-form>
+  </div>
+</template>
